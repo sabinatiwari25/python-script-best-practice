72a3f995283755909e2e4cf70331b8a59e788e71
Sabina Tiwari <sabina.tiwari25@gmail.com>
2026-03-02 15:28:22 -0600

    Putting the new function after the line where we import the sys module.

--- smallest_factor.py
+++ smallest_factor.py
@@ -4,17 +4,21 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
 def get_smallest_prime_factor(n):
     for i in range(2, n):
         if (n % i) == 0:
             return i
     return None
+
+
+if len(sys.argv) != 2:
+    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
+n = int(sys.argv[1])
+if n < 1:
+    sys.exit(sys.argv[0] + ": Expecting a positive integer")
+
+smallest_prime_factor = get_smallest_prime_factor(n)
 
 if smallest_prime_factor is None:
     print(n)
